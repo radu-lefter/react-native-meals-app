d9acc90b01df09bf4c7263f9ee76803c247ff7de
raduubraduu <lefter_radu@yahoo.com>
2023-03-23 18:04:16 +0000

    implemented meal detail screen and navigation functionality

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import MealDetailScreen from './screens/MealDetailScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -23,6 +24,7 @@ export default function App() {
               title: 'All Categories',
             }} />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
