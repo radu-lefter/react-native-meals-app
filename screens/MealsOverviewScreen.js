53b757e765788536679aea2d8f18fb1315d7ddf8
raduubraduu <lefter_radu@yahoo.com>
2023-04-07 22:35:50 +0100

    implemented func to display favourite meals

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,6 +1,5 @@
 import { useLayoutEffect } from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
-import MealItem from '../components/MealItem';
+import MealsList from '../components/MealsList/MealsList';
 
 import { MEALS, CATEGORIES  } from '../data/dummy-data';
 
@@ -22,38 +21,10 @@ function MealsOverviewScreen({ route, navigation  }) {
     });
   }, [catId, navigation]);
 
-  function renderMealItem(itemData) {
-    const item = itemData.item;
-
-    const mealItemProps = {
-      id: item.id, 
-      title: item.title,
-      imageUrl: item.imageUrl,
-      affordability: item.affordability,
-      complexity: item.complexity,
-      duration: item.duration
-    };
     return (
-      <MealItem {...mealItemProps} />
+      <MealsList items={displayedMeals} />
     );
   }
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  );
-}
 
 export default MealsOverviewScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
