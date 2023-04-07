711d65c72ebe7140d6c51cbad6dcf97b3910d979
raduubraduu <lefter_radu@yahoo.com>
2023-04-07 22:09:11 +0100

    completed component to manage context with state

--- store/context/favorites-context.js
+++ store/context/favorites-context.js
@@ -7,9 +7,26 @@ export const FavoritesContext = createContext({
 });
 
 function FavoritesContextProvider({children}) {
+    const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  }
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
   
 
-  return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>
+  return <FavoritesContext.Provider value={value}>{children}</FavoritesContext.Provider>
 }
 
 export default FavoritesContextProvider;
