1f52ad776a65edb1c14ebaf405fad7a2f759d924
raduubraduu <lefter_radu@yahoo.com>
2023-04-07 22:46:34 +0100

    created redux favorites slice

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
+import favoritesReducer from './favorites';
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {favoriteMeals: favoritesReducer}
 });
